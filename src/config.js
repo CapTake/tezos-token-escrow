c7675fb036fde79dbfb1595c22395d25f205ca75
Ant Lion <johndoe@ms.com>
2022-04-23 08:06:00 +0300

    garbage cleaned

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 import { NetworkType } from '@airgap/beacon-sdk'
 const PROJECT_NAME = 'Tez pets'
-const NETWORK = NetworkType.HANGZHOUNET
+const NETWORK = NetworkType.MAINNET
 
 export default {
-  endpoint: NETWORK === NetworkType.MAINNET ? 'https://mainnet.api.tez.ie' : 'https://hangzhounet.api.tez.ie',
+  endpoint: NETWORK === NetworkType.MAINNET ? 'https://mainnet.visualtez.com' : 'https://hangzhounet.api.tez.ie',
   explorer: 'https://tzkt.io',
   tzktapi: NETWORK === NetworkType.MAINNET ? 'https://api.tzkt.io' : 'https://api.hangzhou2net.tzkt.io',
   network: NETWORK, // MAINNET
-  kt: 'KT1LgZqQaK2pi1etXXgadWDCpSZ9JNgGaKFh', // testnet
+  kt: 'KT1Ueg6BTYcj2HBqDARuVTMp5KJjfVSyAQzV', // mainnet
   pageSize: 20,
   pollInterval: 15000,
   projectName: PROJECT_NAME,
